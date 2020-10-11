2d5126ba80429c2a9704ea3f922375ae77965e87
Mohammed Said Al Busaidi <70335350+cyperOM@users.noreply.github.com>
2020-10-11 11:39:28 -0400

    done lab 6

--- lense/gallery.php
+++ lense/gallery.php
@@ -5,5 +5,30 @@ require 'includes/header.php';
 <main>
 <link rel="stylesheet" href="css/gallery.css">
     
+<h1>Gallary</h1>
+<div class="gallary-container">
+    <?php
+include 'includes/dbhandler.php';
+$sql = "SELECT * FROM pictures ORDER BY upload_date DESC";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt,$sql)){
+echo 'SQL Failure';
 
+}else{
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    while($row =mysqli_fetch_assoc($result)){
+   
+        echo '<div class ="card">
+        <a href="#">
+        <img src ="pictures/'.$row["picpath"].'">
+        <h3> '.$row["title"].'</h3>
+        <p>'.$row["discription"].'</p>
+        </a>
+        </div>';
+
+    }
+}
+    ?>
+</div>
 </main>
--- lense/includes/gallery-helper.php
+++ lense/includes/gallery-helper.php
@@ -5,45 +5,46 @@ require 'dbhandler.php';
 define('MB',1048576);
 if (isset($_POST['gallary-submit'])) {
     $Uname = $_SESSION['Uname'];
-    $file = $_FILES['prof-image'];
+    $file = $_FILES['gallary-image'];
     $file_name = $file['name'];
     $file_temp_name = $file['temp_name'];
     $file_error = $file['error'];
     $file_size = $file['size'];
+    $title = $_POST['title'];
 
-    $bio=  $_POST['bio'];
+    $discription=  $_POST['discription'];
 
     $ext = strtolower(pathinfo($file_name,PATHINFO_EXTENSION));
 
       $allowed = array('jpg','jpeg','png','svg');
 
       if ($file_error !==0 ) {
-          header("Location: ../profile.php?error=uploadError");
+          header("Location: ../admin.php?error=uploadError");
           exit();
       } if(!in_array($ext,$allowed)){
-        header("Location: ../profile.php?error=InvalidType");
+        header("Location: ../admin.php?error=InvalidType");
         exit();
       }
       if ($file_size > 4*MB ) {
-        header("Location: ../profile.php?error=LargFile");
+        header("Location: ../admin.php?error=LargFile");
         exit();
       }
       else{
           $new_name  = uniqid('',true).".".$ext;
           $desination = '../uploads/'.$new_name;
-          $sql ="UPDATE profile SET picpath=?, bio=? WHERE Uname=? ";
+          $sql ="INSERT INTO pictures (title,discription,picpath) VALUES (?,?,?)";
           $stmt = mysqli_stmt_init($conn);
           if (!mysqli_stmt_prepare($stmt,$sql)) {
-            header("Location: ../profile.php?error=SqlInjectionAlert");
+            header("Location: ../login.php?error=SqlInjectionAlert");
             exit();
           }
           else {
-            mysqli_stmt_bind_param($stmt,"sss",$desination,$bio,$Uname);
+            mysqli_stmt_bind_param($stmt,"sss",$title,$discription,$uid);
             mysqli_stmt_execute($stmt);
            mysqli_stmt_store_result($stmt);
            move_uploaded_file($file_temp_name,$desination);
 
-           header("Location: ../profile.php?succ=uploadSuccess ");
+           header("Location: ../gallary.php?succ=uploadSuccess ");
             exit();
          
           }
