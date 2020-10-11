dd00819ac4998e7bb82d24a94d18308b05615749
Mohammed Said Al Busaidi <70335350+cyperOM@users.noreply.github.com>
2020-10-11 11:14:18 -0400

    lab 6

--- lense/admin.php
+++ lense/admin.php
@@ -14,10 +14,18 @@ require 'includes/dbhandler.php';
                 <form action="includes/gallary-helper.php" method="POST" enctype="multipart/form-data">
                 <div class ="form-group">
                 <img src="images/atlanticCity.jpg" onclick="triggered();"id="prof-display">
-                <label for="prof-image" id="Uname-style"><?php echo$prof_user;?></label>
-                <input type="file" name ="pro-image" id="prof-image" onchange="preview(this)" class="form-control" style="display: non;">
-                
-                
+                <input type="file" name ="gallary-image" id="gallary-image" onchange="preview(this)" class="form-control" style="display: non;">
+                <input type="text" name="title" id="title" class="form-control" placeholder="Description" require>
+
+                </div>
+                <div class="form-group">
+
+                <textarea name="description" id="discription" cols="30" rows = "3" placeholder="Description"style="text-align: Center;">
+            </textarea>
+                </div>
+                <div class="form-group">
+        <button type="submit" name="gallary-submit" class="btn btn-outline-success btn-lg btn-block">upload</button>
+</div>
                 
                 </div>
                 
--- lense/includes/gallery-helper.php
+++ lense/includes/gallery-helper.php
@@ -1,6 +1,58 @@
 <?php 
 
 require 'dbhandler.php';
-session_start();
+
+define('MB',1048576);
+if (isset($_POST['gallary-submit'])) {
+    $Uname = $_SESSION['Uname'];
+    $file = $_FILES['prof-image'];
+    $file_name = $file['name'];
+    $file_temp_name = $file['temp_name'];
+    $file_error = $file['error'];
+    $file_size = $file['size'];
+
+    $bio=  $_POST['bio'];
+
+    $ext = strtolower(pathinfo($file_name,PATHINFO_EXTENSION));
+
+      $allowed = array('jpg','jpeg','png','svg');
+
+      if ($file_error !==0 ) {
+          header("Location: ../profile.php?error=uploadError");
+          exit();
+      } if(!in_array($ext,$allowed)){
+        header("Location: ../profile.php?error=InvalidType");
+        exit();
+      }
+      if ($file_size > 4*MB ) {
+        header("Location: ../profile.php?error=LargFile");
+        exit();
+      }
+      else{
+          $new_name  = uniqid('',true).".".$ext;
+          $desination = '../uploads/'.$new_name;
+          $sql ="UPDATE profile SET picpath=?, bio=? WHERE Uname=? ";
+          $stmt = mysqli_stmt_init($conn);
+          if (!mysqli_stmt_prepare($stmt,$sql)) {
+            header("Location: ../profile.php?error=SqlInjectionAlert");
+            exit();
+          }
+          else {
+            mysqli_stmt_bind_param($stmt,"sss",$desination,$bio,$Uname);
+            mysqli_stmt_execute($stmt);
+           mysqli_stmt_store_result($stmt);
+           move_uploaded_file($file_temp_name,$desination);
+
+           header("Location: ../profile.php?succ=uploadSuccess ");
+            exit();
+         
+          }
+      }
+
+   
+
+}
+
+
 ?>
 
