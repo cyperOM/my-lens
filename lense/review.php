69a66b65071cb1f5495f1b2231a96a7848efafa7
Mohammed Said Al Busaidi <70335350+cyperOM@users.noreply.github.com>
2020-10-24 23:52:02 -0400

    lab 7

--- lense/review.php
+++ lense/review.php
@@ -6,12 +6,35 @@ require 'includes/review-helper.php';
 
 <main>
     <span id="testAvg"></span>
-    
+    <div class="container" align="center" style="max-width: 800px">
+        <div class="my-auto">
+            <form id="review-form" action="includes/review-helper.php" method="post">
+                <div class="container">
+                    <i class="fa fa-star fa-2x star-rev" data-index="1"></i>
+                    <i class="fa fa-star fa-2x star-rev" data-index="2"></i>
+                    <i class="fa fa-star fa-2x star-rev" data-index="3"></i>
+                    <i class="fa fa-star fa-2x star-rev" data-index="4"></i>
+                    <i class="fa fa-star fa-2x star-rev" data-index="5"></i>
+                </div>
+                <div class="form-group" style="margin-top: 15px;">
+                    <label class="title-label" form="review-title"
+                        style="font-size: 16px; font-width: bold;">Title</label>
+                    <input type="text" name="review-title" id="review-title" style="width: 100%; margin-bottom: 10px;">
+                    <textarea class="form-contrl" id="review-text" name="review" cols="50" rows="3"
+                        placeholder="Enter a comment..."></textarea>
+                    <input type="hidden" name="rating" id="rating">
+                    <input type="hidden" name="item_id" value="<?php echo $_GET['id'];?>">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-outline-danger" id="review-submit" name="review-submit" type="submit"
+                        style="width: 100%;">Review</button>
+                </div>
+            </form>
+        </div>
+    </div>
     <span id="review_list"></span>
 </main>
 <script type="text/javascript">
-
-
 $(document).ready(function() {
     reset_star();
 
@@ -43,11 +66,25 @@ $(document).ready(function() {
 
 
     function reset_star() {
-        $('.star-rev').css('color', 'grey');
+        for (var i = 0; i < max; i++) {
+            $('.star-rev').css('color', 'grey');
+        }
+        document.getElementById('rating').value = parseInt(localStorage.getItem('rating'));
+        console.log(id);
     }
 
 
-    //Used to interchangeably send GET requests for review display data. 
-    
+    //Used to interchangeably send GET requests for review display data.
+    function xhr_getter(prefix, element) {
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById(element).innerHTML = this.responseText;
+            }
+        };
+        url = prefix + id;
+        xhttp.open("GET", url, true);
+        xhttp.sent();
+    }
+
 });
 </script>
